8cab9b63c92571a1cfb5d7f91b97912dfd38ed05
gabriel.silva <gabriel.silva@gotobiz.com.br>
2024-05-27 22:06:18 -0300

    Estrutura inicial de entidades

---
+++ backend/Uniftec.ProjetoWeb.SocialTec/Uniftec.ProjetoWeb.SocialTec.Domain/Repository/IPublicacaoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniftec.ProjetoWeb.SocialTec.Domain.Entities;
+
+namespace Uniftec.ProjetoWeb.SocialTec.Domain.Repository
+{
+    public interface IPublicacaoRepository
+    {
+        void Inserir(Publicacao publicacao);
+        void Alterar(Publicacao publicacao);
+
+        void Excluir(Guid id);
+        Publicacao Procurar(Guid id);
+        List<Publicacao> ProcurarTodos();
+    }
+}
