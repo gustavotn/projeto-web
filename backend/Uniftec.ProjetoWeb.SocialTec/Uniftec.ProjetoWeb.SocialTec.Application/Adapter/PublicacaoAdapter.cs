4ffce402e8166c8c1035506221998438ea973a0e
ggabss1 <gabissantos81@gmail.com>
2024-06-11 11:02:19 -0300

    Testes automatizados e estrutura de API

---
+++ backend/Uniftec.ProjetoWeb.SocialTec/Uniftec.ProjetoWeb.SocialTec.Application/Adapter/PublicacaoAdapter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniftec.ProjetoWeb.SocialTec.Application.Dto;
+using Uniftec.ProjetoWeb.SocialTec.Domain.Entities;
+
+namespace Uniftec.ProjetoWeb.SocialTec.Application.Adapter
+{
+    public static class PublicacaoAdapter
+    {
+        public static Publicacao ToDomain(PublicacaoDto publicacao)
+        {
+            if (publicacao == null)
+                return null;
+            else
+            {
+                Publicacao publicacaoDomain = new Publicacao();
+                publicacaoDomain.Id = publicacao.Id;
+                publicacaoDomain.Usuario = publicacao.Usuario;
+                publicacaoDomain.Descricao = publicacao.Descricao;
+                publicacaoDomain.DataPublicacao = publicacao.DataPublicacao;
+                foreach(var url in publicacao.UrlsMidia)
+                {
+                    publicacaoDomain.AdicionaMidia(url);
+                }
+                return publicacaoDomain;
+            }
+        }
+        public static PublicacaoDto ToDto(Publicacao publicacao)
+        {
+            if (publicacao == null)
+                return null;
+            else
+            {
+                PublicacaoDto publicacaoDto = new PublicacaoDto();
+                publicacaoDto.Id = publicacao.Id;
+                publicacaoDto.Usuario = publicacao.Usuario;
+                publicacaoDto.Descricao = publicacao.Descricao;
+                publicacaoDto.DataPublicacao = publicacao.DataPublicacao;
+                foreach (var url in publicacao.UrlsMidia)
+                {
+                    publicacaoDto.UrlsMidia.Add(url);
+                }
+                return publicacaoDto;
+            }
+        }
+    }
+}
---
+++ backend/Uniftec.ProjetoWeb.SocialTec/Uniftec.ProjetoWeb.SocialTec.Test/PublicacaoRepositoryTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uniftec.ProjetoWeb.SocialTec.Domain.Entities;
+using Uniftec.ProjetoWeb.SocialTec.Repository.Repository;
+
+namespace Uniftec.ProjetoWeb.SocialTec.Test
+{
+    [TestClass]
+    public class ProdutoRepositoryTest
+    {
+        [TestMethod]
+        public void InserirTest()
+        {
+            string strConexao = ""; //Incluir string de conexão
+            var publicacaoRepository = new PublicacaoRepository(strConexao);
+            try
+            {
+                Publicacao publicacao = new Publicacao();
+                publicacao.Id = Guid.NewGuid();
+                publicacao.Usuario = "Gabriel";
+                publicacao.Descricao = "Teste descricao";
+                publicacao.DataPublicacao = DateTime.Now;
+                publicacao.AdicionaMidia("https://google.com.br");
+                publicacaoRepository.Inserir(publicacao);
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void AlterarTest()
+        {
+            string strConexao = ""; //Incluir string de conexão
+            var publicacaoRepository = new PublicacaoRepository(strConexao); try
+            {
+                Publicacao publicacao = new Publicacao();
+                publicacao.Id = Guid.NewGuid(); //Substituir por um ID existente
+                publicacao.Usuario = "Jorge";
+                publicacao.Descricao = "Teste alteracao";
+                publicacao.DataPublicacao = DateTime.Now;
+                publicacao.AdicionaMidia("https://www.google.com.br");
+                publicacaoRepository.Alterar(publicacao);
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ProcurarTest()
+        {
+            string strConexao = ""; //Incluir string de conexão
+            var publicacaoRepository = new PublicacaoRepository(strConexao);
+            try
+            {
+                var publicacao = publicacaoRepository.Procurar(Guid.NewGuid()); //Substituir por um ID existente
+                if (publicacao.Descricao == "Teste descricao")
+                {
+                    Assert.IsTrue(true);
+                }
+                else
+                {
+
+                    Assert.IsTrue(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ProcurarTodosTest()
+        {
+            string strConexao = ""; //Incluir string de conexão
+            var publicacaoRepository = new PublicacaoRepository(strConexao);
+            try
+            {
+                var publicacoes = publicacaoRepository.ProcurarTodos();
+                Assert.IsTrue(true);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
